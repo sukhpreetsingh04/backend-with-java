874a4ea385be18d5867339ab9aa12eb4a6ac0b7d
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2025-05-09 16:18:24 +0530

    changes

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+// package com.example.demo;
 
 // import org.springframework.boot.SpringApplication;
 // import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +13,22 @@ package com.example.demo;
 // }
 
 
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
 public class DemoApplication {
+
     public static void main(String[] args) {
-        System.out.println("Backend is running!");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Java Backend!";
     }
 }
